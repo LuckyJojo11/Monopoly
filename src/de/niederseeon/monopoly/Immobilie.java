f32c319b4270ec70e772d6ed8d173b8ea5b66b9f
snb64 <snb64@MONTE-SNB-64.mshome.net>
2023-12-06 15:22:57 +0100

    Javadoc

--- src/de/niederseeon/monopoly/Immobilie.java
+++ src/de/niederseeon/monopoly/Immobilie.java
@@ -1,3 +1,17 @@
 package de.niederseeon.monopoly;
+
+/**
+ * Klasse Immobilie. Eigentlich keine Immobilie im klassischen Sinn,
+ * sondern ein kaufbares Feld, z.B Straßen, Bahnhöfe,...
+ */
 public class Immobilie {
+	
+
+	/**
+	 * Konstruktor für Immobilien.
+	 */
+	public Immobilie() {
+		
+	}
 }
+
